08a5d060e48c908b8bc27a35541edb34137987c9
Cactus <you@example.com>
2022-12-02 00:01:29 -0600

    Added planned algorithm for queue

---
+++ Queue/queueOP.py
@@ -0,0 +1,7 @@
+# Queue Class
+    # Initialize queue
+    # Check if queue full
+    # Check if queue empty
+    # Add element into queue
+    # Remove element from queue
+    # Display queue
---
+++ Queue/userQueue.py
@@ -0,0 +1,9 @@
+# Create Queue
+# While loop
+    # Display user operations
+    # Request user operation input
+    # Match user choice with selectecd operation and perform operation
+        # Enqueue
+        # Dequeue
+        # Display Queue
+        # Exit
