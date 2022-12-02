a5ab066dca9ef419d0c4999e0ed1bc1f6e87be20
Cactus <you@example.com>
2022-12-02 00:24:57 -0600

    Added UI

--- Queue/userQueue.py
+++ Queue/userQueue.py
@@ -1,8 +1,14 @@
 import queueOP
 
 # Create Queue
+userQueue = Queue()
+op = None
 # While loop
+while op is not 4:
     # Display user operations
+    print("New Queue has been created.\n---------------------------\nSelect an Option:\n")
+    print("1. Add to Queue.\n2. Remove from Queue.\n3. Display Queue\n4. Exit\n")
+    print("Operation: ")
     # Request user operation input
     # Match user choice with selectecd operation and perform operation
         # Enqueue
