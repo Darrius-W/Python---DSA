e7e2ab2b646bee9ca348cdaea2914bd12733666e
Darrius-W <you@example.com>
2022-12-02 00:44:12 -0600

    Add method to add value to Queue

--- Queue/queueOP.py
+++ Queue/queueOP.py
@@ -2,21 +2,31 @@ maxSize = 8 # max size of queue
 
 # Queue Class
 class Queue:
+
     # Initialize queue
     def __init__(self):
         self.queue = []
+
     # Check if queue full
     def isFull(self):
         if len(self.queue) >= 8:
             print("Queue is Full!\n")
             return True
         return False
+
     # Check if queue empty
     def isEmpty(self):
         if len(self.queue) == -1:
             print("Queue is Empty!\n")
             return True
         return False
+
     # Add element into queue
+    def enqueue(self, value):
+        if Queue.isFull(self.queue): # check if que is full
+            pass
+        else:
+            self.queue.append(value)
+
     # Remove element from queue
     # Display queue
