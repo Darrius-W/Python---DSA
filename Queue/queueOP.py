778fc93db9f05ef153327d16ef8a0096be2a3a8e
Cactus <you@example.com>
2022-12-02 00:32:17 -0600

    Added method to check if queue full

--- Queue/queueOP.py
+++ Queue/queueOP.py
@@ -1,9 +1,16 @@
+maxSize = 8 # max size of queue
+
 # Queue Class
 class Queue:
     # Initialize queue
     def __init__(self):
         self.queue = []
     # Check if queue full
+    def isFull(self):
+        if len(self.queue) >= 8:
+            print("Queue is Full!\n")
+            return True
+        return False
     # Check if queue empty
     # Add element into queue
     # Remove element from queue
