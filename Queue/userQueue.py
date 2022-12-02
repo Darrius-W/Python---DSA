944606becd77bc8c7b7a4b74e03f2d136e95645d
Darrius-W <you@example.com>
2022-12-02 01:01:49 -0600

    Added request for UI operation input

--- Queue/userQueue.py
+++ Queue/userQueue.py
@@ -11,7 +11,13 @@ while op is not 4:
     print("---------------------------\nSelect an Option:\n")
     print("1. Add to Queue.\n2. Remove from Queue.\n3. Display Queue\n4. Exit\n")
     print("Operation: ")
+    
     # Request user operation input
+    try:
+        op = input(range(1,4))
+    except:
+        print("*** Error Invalid Input! ***\n")
+        pass
     # Match user choice with selectecd operation and perform operation
         # Enqueue
         # Dequeue
